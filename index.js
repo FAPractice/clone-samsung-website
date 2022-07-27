6bd593a82c25094b35855fc0dff260020c2b4fa0
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-07-27 20:17:02 +0530

    Nav Menu Animation, Autoclose

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
+const navMenu = document.getElementById("nav-menu");
+
+const closeAnimationFunction = (duration) => {
+  navMenu.animate(
+    [
+      {
+        boxShadow: "0px 0px 0px 100vmax #00000000",
+        transform: "translateX(100%)",
+      },
+    ],
+    {
+      duration: duration,
+    }
+  ),
+    setTimeout(() => navMenu.classList.remove("visible"), duration);
+};
+
 document.querySelectorAll(".btn-nav-menu").forEach((element) => {
   element.onclick = () => {
-    document.getElementById("nav-menu").classList.toggle("visible");
+    navMenu.classList.contains("visible")
+      ? closeAnimationFunction(200)
+      : navMenu.classList.add("visible");
   };
 });
+
+window.onresize = () => {
+  if (window.innerWidth > 1200 && navMenu.classList.contains("visible")) {
+    closeAnimationFunction(400);
+  }
+};
